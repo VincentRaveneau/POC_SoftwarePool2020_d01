bc1dea4ad5177acb53b81234bc1dbfbb2420cd4f
vincent raveneau <vincent.raveneau@epitech.eu>
2020-03-02 15:24:43 +0100

    [ADD] -> params header cookie

--- src/index.js
+++ src/index.js
@@ -2,39 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-function startServeur() {
-  const app = express();  
-  app.listen(8080);
+function startServer() {
+  const app = express();
   app.use(bodyParser.text());
   app.use(cookieParser());
   app.get('/hello', (req, res) => {
     res.send('world');
   });
-  
+
   app.get('/repeat-my-fixed', (req, res) => {
     res.sendStatus(200);
   });
-  
+
   app.get('/repeat-my-query', (req, res) => {
     const { message } = req.query;
-    if (!message)
-      res.sendStatus(400);
-    else
-      res.send(req.query.message);
+    if (!message) res.sendStatus(400);
+    else res.send(req.query.message);
   });
-  
+
   app.post('/repeat-my-body', (req, res) => {
-    if (!req.body)
-      res.sendStatus(400);
-    else
-      res.send(req.body);
+    if (!req.body) res.sendStatus(400);
+    else res.send(req.body);
   });
   app.get('/repeat-my-cookie', (req, res) => {
-    if (!req.cookies.message)
-      res.sendStatus(400);
-    else
-      res.send(req.cookies.message);
+    if (!req.cookies.message) res.sendStatus(400);
+    else res.send(req.cookies.message);
   });
+  app.get('/repeat-my-header', (req, res) => {
+    if (!req.header('X-Message')) res.sendStatus(400);
+    else res.send(req.header('X-Message'));
+  });
+  app.get('/repeat-my-param/:message', (req, res) => {
+    res.send(req.params.message);
+  });
+  app.listen(8080);
 }
-
-startServeur();
+startServer();
