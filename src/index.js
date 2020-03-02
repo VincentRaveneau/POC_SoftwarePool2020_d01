680fde92519fc4f583886320602315fd5bf82c8a
vincent raveneau <vincent.raveneau@epitech.eu>
2020-03-02 11:37:34 +0100

    [ADD] -> status 200

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 function startServeur() {
   const express = require('express');
   const app = express();
-  app.listen(8000);
+  app.listen(8080);
   app.get('/hello', (req, res) => {
     res.send('world');
+  });
+  app.get('/repeat-my-fixed', (req, res) => {
+    res.sendStatus(200);
   });
 }
 
